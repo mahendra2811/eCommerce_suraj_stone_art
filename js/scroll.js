fa821e74d20d50ba41958bdb0d3134d45fd8bfc7
Mahendra Singh Puniya <mahendrapuniya92@gmail.com>
2024-02-21 09:49:54 +0530

    third commit

--- js/scroll.js
+++ js/scroll.js
@@ -31,3 +31,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // photo grid js end
 
+
+// for pdf download start 
+function downloadCustomCatalog() {
+    // Replace 'path-to-your-catalog.pdf' with the actual path to your PDF file
+    const pdfPath = '../images/Catelog_suraj_stone_art.pdf';
+  
+    // Create an anchor element
+    const link = document.createElement('a');
+  
+    // Set the href attribute to the path of the PDF file
+    link.href = pdfPath;
+  
+    // Set the download attribute to specify the default filename
+    link.download = 'custom-catalog.pdf';
+  
+    // Append the link to the document
+    document.body.appendChild(link);
+  
+    // Trigger a click on the link to start the download
+    link.click();
+  
+    // Remove the link from the document
+    document.body.removeChild(link);
+}
+// for pdf download end 
+
+
